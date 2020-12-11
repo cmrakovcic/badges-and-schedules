09b02acd9b5cd2ec4a1d962fdfecd1372596e617
Christopher Mrakovcic <cmrakovcic@aol.com>
2020-12-10 19:05:19 -0500

    commit tree 587472b95e583ceb957e91b0a710185bc3054df6

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) {|name, x| "Hello, #{name}! You'll be assigned to room #{x}!"}
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+
+assign_rooms(names).each do |message|
+    puts message
+    end
+end
+
+
